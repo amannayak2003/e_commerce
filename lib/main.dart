6529688205bbf5a2eda166787533281ea2373f0b
Aman Nayak <amannayak774@gmail.com>
2024-07-13 18:09:42 +0530

    splash screen is Added
    -animation json file for splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:e_commerce/splash1.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,17 +11,35 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const Color primary = Color(0xFF074278);
+  static const Color secondary = Color(0xFFF5D3D3);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+          textTheme: const TextTheme(
+              displayLarge: TextStyle(
+                  fontSize: 20,
+                  letterSpacing: 3,
+                  fontFamily: 'myfont1',
+                  fontWeight: FontWeight.w800,
+                  color: Colors.black),
+              titleMedium: TextStyle(
+                  fontSize: 12,
+                  letterSpacing: 1,
+                  fontFamily: 'myfont1',
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black))),
+      home: AnimatedSplashScreen(
+        splash: Center(child: Lottie.asset("assets/animation/splash.json")),
+        nextScreen: home(),
+        duration: 3500,
+        splashIconSize: 200,
+        backgroundColor: MyApp.secondary,
       ),
-      home: splash1()
     );
   }
-
 }
